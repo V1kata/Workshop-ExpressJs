53ae532500ca3c31e3516826525bf057b5984821
V1kata <viktor.burboran@gmail.com>
2023-01-27 17:38:12 +0200

    Routes
    Navigation in main, renamed the html to hbs and made the accessory get and post page

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
+const accessoryController = require('./controllers/accessoryController');
 const router = require('express').Router(); // var 1
 
 // const express = require('express');
@@ -19,6 +20,10 @@ router.route('/create')
     .post(cubeController.postCreateCube)
 
 router.get('/details/:cubeId', cubeController.getDetails);
+
+router.route('/create/accessory')
+    .get(accessoryController.getAccessoryCreate)
+    .post(accessoryController.postAccessory);
 
 router.get('/404', homeController.getNotFound);
 
