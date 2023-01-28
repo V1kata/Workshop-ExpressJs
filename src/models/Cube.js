f3b49dd4b9e2d97c9cfc59f1718cbb3301313ce9
V1kata <viktor.burboran@gmail.com>
2023-01-28 19:25:47 +0200

    Max length is more

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -8,7 +8,7 @@ const cubeSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
-        maxLength: 50
+        maxLength: 60
     },
     imageUrl: {
         type: String,
