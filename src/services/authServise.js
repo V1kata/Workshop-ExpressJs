07724ae2ad800a69f81ca3f188905c2957853ef0
V1kata <viktor.burboran@gmail.com>
2023-02-08 20:07:22 +0200

    Error class to handle login errors

--- src/services/authServise.js
+++ src/services/authServise.js
@@ -1,6 +1,7 @@
 const config = require("../config/config");
 const User = require("../models/User");
 const jwt = require('../config/jwt');
+const AppError = require('../utils/appError');
 
 exports.getUser = (username) => User.findOne({ username });
 
@@ -9,10 +10,14 @@ exports.register = (username, password) => User.create({ username, password });
 exports.login = async (username, password) => {
     const user = await this.getUser(username);
 
+    if (!user) {
+        throw new AppError('Invalid username', { user });
+    }
+
     const isValid = await user.validatePassword(password);
 
-    if (!isValid || !user) {
-        throw new Error('Invalid username or password');
+    if (!isValid) {
+        throw new AppError('Invalid password');
     }
 
     const payload = { _id: user._id, username };
